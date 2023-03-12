fad8bade008d7e68cc3c49fbda768ca373ed2e85
Kimbumsoo99 <show7441@naver.com>
2023-03-13 05:41:50 +0900

    연습

--- server/app.js
+++ server/app.js
@@ -4,6 +4,20 @@ const app = express();
 const port = 5000;
 const appUrl = `http://localhost:${port}`;
 
+const ex0 = (req, res, next) => {
+  console.log("첫 번째 콜백 함수");
+  next();
+};
+
+const ex1 = (req, res, next) => {
+  console.log("두 번째 콜백 함수");
+  next();
+};
+
+const ex2 = (req, res, next) => {
+  res.send("세 번째 콜백 함수");
+};
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -26,7 +40,7 @@ app.get(
     res.send("두 번째 콜백 함수");
   }
 );
-
+app.get("/examples", [ex0, ex1, ex2]);
 app.all("/customer/:id", (req, res) => {
   res.send("HTTP 요청 메소드 종류에 상관없이 응답");
 });
