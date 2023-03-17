ca7514a7d0d291de21c8df22d9df8b13a49cccfd
Kimbumsoo99 <show7441@naver.com>
2023-03-18 08:27:25 +0900

    연습

--- db_server/mysql/index.js
+++ db_server/mysql/index.js
@@ -1,13 +1,14 @@
 import mysql from "mysql";
 import sql from "./sql";
+require("dotenv").config({ path: "mysql/.env" });
 
 const pool = mysql.createPool({
-  connectionLimit: 10,
-  host: "127.0.0.1",
-  port: 3306,
-  user: "dev01",
-  password: "1234",
-  database: "dev",
+  connectionLimit: process.env.MYSQL_LIMIT,
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USERNAME,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB,
 });
 
 const query = async (alias, values) => {
